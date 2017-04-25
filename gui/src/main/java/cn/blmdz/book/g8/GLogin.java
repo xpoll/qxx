f9561c53b49ab8b5624793c7700f3e6f4455952f
xpoll <blmdz521@126.com>
2017-04-25 22:40:41 +0800

    a

---
+++ gui/src/main/java/cn/blmdz/book/g8/GLogin.java
@@ -0,0 +1,126 @@
+package cn.blmdz.book.g8;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import cn.blmdz.book.g8.JdbcUtil.CallBackNoResult;
+
+/**
+ * @author lm
+ * 登陆
+ */
+public class GLogin extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+
+	private JButton loginBtn, clearBtn, registerBtn, resetBtn, outBtn;
+	private JLabel usernameLabel, passwordLabel;
+	private JTextField usernameText;
+	private JPasswordField passwordText;
+	private JDialog msg;
+	private int massage = 0;
+	
+	public GLogin() {
+		super("电信IP资费管理系统");
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		usernameLabel = new JLabel("用户姓名");
+		passwordLabel = new JLabel("用户口令");
+		usernameText = new JTextField(10);
+		passwordText = new JPasswordField(10);
+		
+		loginBtn = new JButton("登录");
+		clearBtn = new JButton("清除");
+		registerBtn = new JButton("注册");
+		resetBtn = new JButton("重新输入");
+		outBtn = new JButton("退出");
+		
+		loginBtn.addActionListener(this);
+		clearBtn.addActionListener(this);
+		registerBtn.addActionListener(this);
+		resetBtn.addActionListener(this);
+		outBtn.addActionListener(this);
+		
+		c.add(usernameLabel);
+		c.add(usernameText);
+		c.add(passwordLabel);
+		c.add(passwordText);
+		
+		c.add(loginBtn);
+		c.add(clearBtn);
+		c.add(registerBtn);
+		c.add(new JLabel("如果你还没有注册，请注册"));
+		
+		msg = new JDialog();
+		msg.setSize(340, 80);
+		msg.setLocation(this.getX() + 100, this.getY() + 100);
+		msg.setLayout(new FlowLayout());
+		msg.add(new JLabel("重新输入还是退出？"));
+		msg.add(resetBtn);
+		msg.add(outBtn);
+
+		this.setSize(200, 200);
+		this.setLocation(360, 240);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == loginBtn) {
+			final String sql = "select * from t_consumer";
+			JdbcUtil.connection(new CallBackNoResult() {
+				@Override
+				public void execute(Statement stmt, ResultSet rs) throws SQLException {
+					rs = stmt.executeQuery(sql);
+					while (rs.next()) {
+						String id = rs.getString("id");
+						String password = rs.getString("password");
+						char[] ps = passwordText.getPassword();
+						String str = "";
+						for (char c : ps) {
+							str += c;
+						}
+						if (id.equals(usernameText.getText()) && password.equals(str)) {
+							massage = 1;
+							// TODO 新建菜单
+							break;
+						}
+					}
+				}
+			});
+			if (massage == 0) {
+				JOptionPane.showMessageDialog(this, "账号或密码有误", "系统提示", JOptionPane.ERROR_MESSAGE);
+			} else if (massage == 1) {
+				setVisible(false);
+			}
+			
+		} else if (e.getSource() == clearBtn) {
+			
+		} else if (e.getSource() == registerBtn) {
+			
+		} else if (e.getSource() == resetBtn) {
+			
+		} else if (e.getSource() == outBtn) {
+			
+		}
+	}
+	public static void main(String[] args) {
+		new GLogin();
+	}
+}
---
+++ gui/src/main/java/cn/blmdz/book/g8/JdbcUtil.java
@@ -0,0 +1,65 @@
+package cn.blmdz.book.g8;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcUtil {
+	public static void connection(CallBackNoResult callBack) {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_test", "root", "root");
+			stmt = conn.createStatement();
+			callBack.execute(stmt, rs);
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException");
+		} catch (SQLException e) {
+			System.out.println("SQLException");
+		} finally {
+			try {
+				if (stmt != null) stmt.close();
+				if (conn != null) conn.close();
+				if (rs != null) rs.close();
+			} catch (SQLException e) {
+				System.out.println("SQLException");
+			}
+		}
+	}
+
+	public static <T> T connection(CallBack<T> callBack) {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_test", "root", "root");
+			stmt = conn.createStatement();
+			return callBack.execute(stmt, rs);
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException");
+		} catch (SQLException e) {
+			System.out.println("SQLException");
+		} finally {
+			try {
+				if (stmt != null) stmt.close();
+				if (conn != null) conn.close();
+				if (rs != null) rs.close();
+			} catch (SQLException e) {
+				System.out.println("SQLException");
+			}
+		}
+		return null;
+	}
+	
+	public interface CallBackNoResult {
+		void execute(Statement stmt, ResultSet rs) throws SQLException;
+	}
+	public interface CallBack<T> {
+		T execute(Statement stmt, ResultSet rs) throws SQLException;
+	}
+}
